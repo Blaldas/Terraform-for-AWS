bd63e3127aa75d5fb446d3cce6985a1c56154904
Blaldas <marmingues00@gmail.com>
2023-01-11 22:04:39 +0000

    added elastic ip demo

---
+++ eip/main.tf
@@ -0,0 +1,24 @@
+locals{
+    access_file = jsondecode(file("${path.module}/../access.json"))
+    access_key = local.access_file.Access_key_ID
+    secret_key = local.access_file.Secret_access_Key
+}
+
+provider "aws" {
+    region="eu-west-1"
+    access_key = local.access_key
+    secret_key = local.secret_key
+}
+
+resource "aws_instance" "ec2" {
+    ami = "ami-0fe0b2cf0e1f25c8a"
+    instance_type = "t2.micro"
+}
+
+resource "aws_eip" "eip" {
+    instance = aws_instance.ec2.id
+}
+
+output "eip" {
+    value = aws_eip.eip.public_ip
+}
