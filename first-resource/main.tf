1bd7245eb2579d2ad682892589c1f1a5ae1ca70c
Blaldas <marmingues00@gmail.com>
2023-01-11 21:48:04 +0000

    updated fist resource

--- first-resource/main.tf
+++ first-resource/main.tf
@@ -1,6 +1,13 @@
-provider "aws"{
-    region= "eu-west-1"
+locals{
+    access_file = jsondecode(file("${path.module}/../access.json"))
+    access_key = local.access_file.Access_key_ID
+    secret_key = local.access_file.Secret_access_Key
+}
 
+provider "aws" {
+    region="eu-west-1"
+    access_key = local.access_key
+    secret_key = local.secret_key
 }
 
 resource "aws_vpc" "myvpc"{
